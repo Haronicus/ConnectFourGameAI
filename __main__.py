39a43573c012577d3213ad7b8ecd7b4fec9c721e
Haronicus <michael.smith@wyakin.org>
2023-03-04 19:45:33 -0700

    Initial commit

---
+++ __main__.py
@@ -0,0 +1,13 @@
+from ConnectFour import *
+from PyGamePlayer import *
+from TerminalPlayer import TerminalPlayer
+from StudentPlayer import StudentPlayer
+
+myGame = ConnectFour(render=True)  
+
+
+playerA  = StudentPlayer()    #random bot
+playerB = TerminalPlayer()
+playerC2  = PyGamePlayer(myGame)
+
+myGame.playGame(playerC2,playerC2)
